3a4be2d6fa8e86f3e8362c08f977b36071f51505
Nitin Dixit <nitinajaydixit@gmail.com>
2022-06-03 23:12:20 +0530

    add posts

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,22 @@
 import Head from "next/head";
 import Layout, { siteTitle } from "../components/layout";
 import utilStyles from "../styles/utils.module.css";
+import { getSortedPostsData } from "../lib/posts";
 
-export default function Home() {
+export async function getStaticProps() {
+  const allPostsData = getSortedPostsData();
+  return {
+    props: {
+      allPostsData,
+    },
+  };
+}
+
+export default function Home({
+  allPostsData,
+}: {
+  allPostsData: { id: string }[];
+}) {
   return (
     <Layout home>
       <Head>
@@ -11,6 +25,23 @@ export default function Home() {
       <section className={utilStyles.headingMd}>
         <p>Hi I&apos;m a Frontend Engineer.</p>
         <p>My Tech Stack: HTML, CSS, JS/TS, React/Next, Tailwind</p>
+      </section>
+
+      <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
+        <h2 className={utilStyles.headingLg}>Blog</h2>
+        <ul className={utilStyles.list}>
+          {allPostsData.map(({ id, date, title }: any) => (
+            <li className={utilStyles.listItem} key={id}>
+              <>
+                {title}
+                <br />
+                {id}
+                <br />
+                {date}
+              </>
+            </li>
+          ))}
+        </ul>
       </section>
     </Layout>
   );
